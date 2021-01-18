0a7f179a3e9285417d0c459c3bfffea3117a01b6
Kamil <kamiljanprzybyl@gmail.com>
2021-01-18 09:30:41 +0100

    ft_strdup updated

--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -2,26 +2,19 @@
 
         extern  _malloc
         extern  _ft_strlen
+        extern  _ft_strcpy
 
 _ft_strdup:
         push    rdi             ; push rdi's value onto the stack as it will be destroyed after call
         call    _ft_strlen      ; check string lenght
         mov     rdi, rax        ; pass parameter to malloc, which is number of bytes to allocate
         inc     rdi             ; one byte additionaly for null character
-        call    _malloc             
-        mov     r9, rax         ; save pointer to the allocated memory
+        call    _malloc         
         pop     rdi             ; pop rdi's initial value from the stack
-        jmp     copy
 
-copy:
-        mov     dl, [rdi]
-        mov     [rax], dl
-        cmp     dl, 0
-        jz      return
-        inc     rdi
-        inc     rax
-        jmp     copy
+        mov     rsi, rdi        
+        mov     rdi, rax
+        call    _ft_strcpy
 
 return:
-        mov     rax, r9
         ret
